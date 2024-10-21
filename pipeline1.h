a63955dc7c7a0d117dc8affd593486f8b686bac1
ladev789 <xlwmail@nudt.edu.cn>
2024-10-21 23:07:27 +0800

    利用管道实现了多线程通信，可以跑了，但是少了大约200条指令

---
+++ pipeline1.h
@@ -0,0 +1,54 @@
+#include "./regfile.c"
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define MAXINST 4096 // 假设最大指令数为10000，根据需要调整
+
+// 定义数据类型
+#define ALU 0x01
+#define BRANCH 0x02
+#define JMP 0x04
+#define LOAD 0x08
+#define STORE 0x10
+#define FPU 0x20
+#define ALU_cnt 4
+#define FPU_cnt 4
+
+// 定义CPU的状态枚举
+typedef enum { IF, ID, EX, MEM, WB } State;
+
+// 定义指令结构体
+struct instr {
+  char *name; // 指令名称
+  dword code; // 指令代码
+  int opcode; // 操作码
+  int fmt;    // 指令格式
+  int base, rs, rt, rd, fs, ft, fd, shamt, func;
+  word imm;            // 立即数
+  int offset;          // 偏移量
+  unsigned int nextpc; // 下一条指令的PC地址
+  qword base_v, rs_v, rt_v, rd_v, fs_v, ft_v, fd_v; // 操作数的值
+};
+
+struct INST {
+  char opcode;
+  char RS1, RS2, Rd;
+  int Imm;
+  bool Flush;
+  bool Hlt;
+};
+
+struct STAGE {
+  bool valid;
+  struct INST inst;
+};
+
+// 错误处理函数声明
+void handle_decode_error(dword);
+void handle_excute_error(dword);
+
+// 全局变量
+extern struct INST INSTLIST[MAXINST];
+extern struct INST nop;
+// extern struct STAGE StageF, StageD, StageE, StageM, StageW;
